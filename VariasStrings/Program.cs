19879aef11e73d3037c5ef27b4881431c75bdb69
Pedro-Coutinho <pedro_coutinho18@outlook.com>
2020-02-27 18:11:04 +0000

    Formating

--- VariasStrings/Program.cs
+++ VariasStrings/Program.cs
@@ -6,14 +6,19 @@ namespace VariasStrings
     {
         static void Main(string[] args)
         {
+            double xx = 0.12345;
+            int ii = 18;
+
+            //Normal Strings
             string normal = "Isto é uma string normal";
             string codigos = "Isto é uma aspa \", isto é um código \u0001";
             string verbatim = @"Literalmente o que está escrito ||||";
-
+            //Print Normal strings
             Console.WriteLine(normal);
             Console.WriteLine(codigos);
             Console.WriteLine(verbatim);
 
+            //String Fotmating
             int numero = 64;
             int numero1 = 70;
             string a = "Abc";
@@ -22,11 +27,17 @@ namespace VariasStrings
             string b = String.Format("Valor de x = {0},  valor de y = {1}", 
             numero, numero1);
 
+            //Print formated strings
             Console.WriteLine(a);
             Console.WriteLine(b);
             Console.WriteLine(c);
             Console.WriteLine(d);
 
+            string numero2CasasDecimaisEPercentagem = $"x em decimal = {xx:f2}, x em percentagem = {xx:p1}";
+            string numerodoii = $"ii em hexadecimal = {ii:X}, em moeda {ii:C}";
+
+            Console.WriteLine(numero2CasasDecimaisEPercentagem);
+            Console.WriteLine(numerodoii);
 
         }
     }

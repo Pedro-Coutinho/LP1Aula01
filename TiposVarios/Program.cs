fb858cc60dde0689e3a48fbb1aa1b050e35d01d2
Pedro-Coutinho <pedro_coutinho18@outlook.com>
2020-02-27 17:12:41 +0000

    NumerosInteiros

---
+++ TiposVarios/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TiposVarios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = 1;
+            int numero1 = 0;
+            int numero2 = 54;
+            int numero3 = 21;
+            
+            Console.WriteLine(numero);
+            Console.WriteLine(numero1);
+            Console.WriteLine(numero2);
+            Console.WriteLine(numero3);
+        }
+    }
+}

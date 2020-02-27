02a57dbff1706e3bdd93b1c54ce41286bc2012eb
Pedro-Coutinho <pedro_coutinho18@outlook.com>
2020-02-27 17:38:07 +0000

    variasstrings

---
+++ VariasStrings/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace VariasStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string normal = "Isto é uma string normal";
+            string codigos = "Isto é uma aspa \", isto é um código \u0001";
+            string verbatim = @"Literalmente o que está escrito ||||";
+
+            Console.WriteLine(normal);
+            Console.WriteLine(codigos);
+            Console.WriteLine(verbatim);
+
+        }
+    }
+}

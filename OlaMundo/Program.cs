e6c05f826a71b03fc6fbf7fdd3c26fcd37f41546
Pedro-Coutinho <pedro_coutinho18@outlook.com>
2020-02-27 16:33:38 +0000

    Add Summary

--- OlaMundo/Program.cs
+++ OlaMundo/Program.cs
@@ -2,8 +2,17 @@
 
 namespace OlaMundo
 {
+    /// <summary>
+    /// My first C# program
+    /// </summary>
     class Program
     {
+        /// <summary>
+        /// Print Hello and Goobye world on console
+        /// </summary>
+        /// <param name="args"></param>
+        /// Command-line arguments
+        /// </param>
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");

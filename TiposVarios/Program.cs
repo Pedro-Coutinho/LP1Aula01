5eb3d3be1a8a8e45c8a0ded897dcd1fe62bdee18
Pedro-Coutinho <pedro_coutinho18@outlook.com>
2020-02-27 17:17:01 +0000

    NumerosInteiros

--- TiposVarios/Program.cs
+++ TiposVarios/Program.cs
@@ -10,11 +10,21 @@ namespace TiposVarios
             int numero1 = 0;
             int numero2 = 54;
             int numero3 = 21;
-            
+
             Console.WriteLine(numero);
             Console.WriteLine(numero1);
             Console.WriteLine(numero2);
             Console.WriteLine(numero3);
+            
+            char unicode1 ='\u0041' ;
+            char unicode2 ='\u02A9' ;
+            char unicode3 ='\u058D' ;
+            char unicode4 ='\u0AE6' ;
+
+            Console.WriteLine(unicode1);
+            Console.WriteLine(unicode2);
+            Console.WriteLine(unicode3);
+            Console.WriteLine(unicode4);
         }
     }
 }
